a7b0cf29583781129e18f08919dd063dbbbdcd11
Jacob <perezjacob2@gmail.com>
2021-01-25 11:29:39 -0600

    Revert "API KEY"
    This reverts commit 00cb30953c601e424bbe15ab203890501dbc5962.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const api = {
-    key: config.MY_KEY,
+    key: "eefc306f35c121cc977d10581db95625",
     base: "https://api.openweathermap.org/data/2.5/"  
 }   
 
@@ -25,8 +25,8 @@ function getData () {
 
 function displayData (response) {
     if (response.cod==="404" || search.value==="") {
-        const message = document.querySelector(".message")
-        message.classList.add("error");
+        const message = document.querySelector('.message')
+        message.classList.add('error');
         message.innerHTML = "Please enter a valid city.";
         setTimeout(() => message.remove(), 3000);
         search.value="";
@@ -35,11 +35,11 @@ function displayData (response) {
         city.innerHTML = `${response.name}, ${response.sys.country}`;
 
         const now = new Date();
-        const date = document.querySelector(".date");
+        const date = document.querySelector('.date');
         date.innerHTML = dateBuilder(now);
         
         const temp = document.querySelector(".temp");
-        temp.innerHTML=`Temp: ${Math.round(response.main.temp)+ "°f"}`;
+        temp.innerHTML=`Temp: ${Math.round(response.main.temp)+ '°f'}`;
 
         const humid = document.querySelector(".humidity")
         humid.innerText =`Humidity: ${response.main.humidity}`+"%";
@@ -48,7 +48,7 @@ function displayData (response) {
         descprit.innerHTML = `${response.weather[0].main}`;
 
         const speed = document.querySelector('.wind');
-        speed.innerHTML = `Wind Speed: ${response.wind.speed +"p/h"}`;
+        speed.innerHTML = `Wind Speed: ${response.wind.speed +'p/h'}`;
 
         const weatherIcon = document.querySelector(".icon");
         const iconUrl = "http://openweathermap.org/img/w/";
@@ -65,7 +65,7 @@ document.querySelector(".search-bar").addEventListener("keyup", function (event)
 });
 
 const now = new Date();
-const date = document.querySelector(".date");
+const date = document.querySelector('.date');
 date.innerHTML = dateBuilder(now);
     
 function dateBuilder (d) {
